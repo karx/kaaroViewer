22a994b262b030f024896f7068c5a397f096fa07
Kartik <kartik.arora1214@gmail.com>
2019-09-27 07:34:58 +0530

    sky

--- components/sky-canvas.js
+++ components/sky-canvas.js
@@ -5,102 +5,74 @@ AFRAME.registerComponent('sky-canvas', {
       console.log("InCanvas REady");
       var canvas = this.canvas = document.getElementById(this.data);
       var ctx = this.ctx = this.canvas.getContext('2d');
-        var xMax = this.canvas.width = window.screen.availWidth;
-        var yMax = this.canvas.height = window.screen.availHeight;
-          this.color = 'rgba(255, 255, 255, 1)';
-          this.minRadius = 0.5;
-          this.maxRadius = 1.5;
-          this.minSpeed = .1;
-          this.maxSpeed = .5;
-          this.fps = 60;
-          this.numStars = 400;
-          this.render();
-          this.createCircle();
-      },
+      var STAR_COLOURS = this.STAR_COLOURS = ["#ffffff", "#ffe9c4", "#d4fbff"], // because not all stars are white
+      HEIGHT = 4096, // height of the canvas
+      WIDTH = 4096; // width of the canvas
+      // find the canvas and create its ctx
+    
+    // set up the width and height
+      canvas.width = this.WIDTH =  WIDTH;
+      canvas.height = this.HEIGHT = HEIGHT;
 
-      _rand: function (min, max) {
-          return Math.random() * (max - min) + min;
-      },
-
-      render: function () {
-          var self = this,
-              wHeight = window.innerHeight,
-              wWidth = window.innerWidth;
-
-          self.canvas.width = wWidth;
-          self.canvas.height = wHeight;
-
-          window.addEventListener('resize', self.render);
-      }
-      ,
-      //Create shape
-      createCircle: function () {
-          var star = [];
-
-          for (var i = 0; i < this.numStars; i++) {
-              var self = this;
-              star[i] = {
-                  radius: self._rand(self.minRadius, self.maxRadius),
-                  xPos: self._rand(0, self.canvas.width),
-                  yPos: self._rand(0, self.canvas.height),
-                  xVelocity: self._rand(self.minSpeed, self.maxSpeed),
-                  yVelocity: self._rand(self.minSpeed, self.maxSpeed),
-                  color: self.color
-              }
-
-              //once values are determined, draw star on canvas
-              self.draw(star, i);
-          }
-          //...and once drawn, animate the star
-          self.animate(star);
-      }
-      ,
-     // Draw the stars on canvas
-      draw: function (star, i) {
-          var self = this,
-              ctx = self.ctx;
-              ctx.fillStyle = star[i].color;
-
-          ctx.beginPath();
-          ctx.arc(star[i].xPos, star[i].yPos, star[i].radius, 0, 2 * Math.PI, false);
-          ctx.fill();
-          this.updateTexture();
-      }
-      ,
-      // Animate stars from right to left
-      animate: function (star) {
-          var self = this,
-              ctx = self.ctx;
-
-          setInterval(function () {
-              //clears canvas
-              self.clearCanvas();
-              //redraws stars
-              for (var i = 0; i < self.numStars; i++) {
-                  star[i].xPos -= star[i].xVelocity;
-                  //if star goes off screen from the left call reset method
-                  if (star[i].xPos < 0) {
-                      self.resetStar(star, i);
-                  }
-                  else{
-                      self.draw(star, i);
-                  }
-              }
-          }, 1000 / self.fps);
-      }
-      ,
-      // reset the star xPos with a randon YPos 
-      resetStar: function (star, i) {
-          var self = this;
-          star[i].xPos +=  self.canvas.width + star[i].radius;
-          star[i].yPos = self._rand(0, self.canvas.height);
-          self.draw(star, i);
-      },
-
-      clearCanvas: function () {
-          this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      }
-      
+      // create a star field
+      this.star_field(ctx, 300);
+    },
+    // create a new star field when you click on the canvas
+    // canvas.addEventListener ("click", function () {
+    //   star_field(ctx, 200);
+    // }, false);
+  
+  /**
+   * Generate a random integer between min and max
+   */
+  random: function(min, max) {
+    return Math.round((Math.random() * max - min) + min);
+  }, 
+  
+  /**
+   * Generate a new star field with star_number stars and draw 
+   * it onto the provided canvas ctx
+   */
+  star_field: function (ctx, star_number) {
+    var x, // x position of the star
+        y, // y position of the star
+        brightness, // brightness of the star
+        radius; // radius of the star
+  
+    // draw the blank night sky
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
+    
+    // save the previous canvas ctx state
+    ctx.save();
+    
+    for (var i = 0; i < star_number; i++) {
+      x = Math.random() * this.WIDTH; // random x position
+      y = Math.random() * this.HEIGHT; // random y position
+      radius = Math.random() * 1.1; // random radius
+      brightness = this.random(80, 100) / 100;
+  
+      // start drawing the star
+      ctx.beginPath();
+      // set the brightness of the star
+      ctx.globalAlpha = brightness;
+      // choose a random star colour
+      ctx.fillStyle = this.STAR_COLOURS[this.random(0, this.STAR_COLOURS.length)];
+      // draw the star (an arc of radius 2 * pi)
+      ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+      // fill the star and stop drawing it
+      ctx.fill();
+      ctx.closePath();
+    }
+    
+    // restore the previous ctx state
+    ctx.restore();
+  }
+  
+  /**
+   * Kick everything off
+   */
+  
   });
 
   
